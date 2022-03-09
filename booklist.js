9407ec91a5af6417e57b7a72d5ebb4de450f4596
GergoKovacsBookr <kiskovi97@gmail.com>
2022-03-09 11:51:44 +0100

    list

--- booklist.js
+++ booklist.js
@@ -2,41 +2,49 @@ var bookList = [{
     id: "97",
     src: "https://bkrapi-win.v2.bookrclass.com/convertedBooks/ID97_20211112130024.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci93LzMvdzNsa3p5ZzFZYW1pQjlxVXJMYU1vSFZseDU1UXJUeGhVT1VvbkVQWUs0LmpwZw::/original_4k.jpg",
+    name: "Gondri, the Black Dog",
 },
 {
     id: "517",
     src: "https://bkrapi-win.v2.bookrclass.com/convertedBooks/ID517bc_20220124165403.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci84L0IvOEJkdnVjRllFaXFTbWFoWkZVclJBQUN4UUdsaURuVXNsdGRSeXhkQkl2LmpwZw::/original_4k.jpg",
+    name: "Follow Me",
 },
 {
     id: "378",
     src: "https://bkrapi-win.v2.bookrclass.com/convertedBooks/ID378_20211116124218.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci9RL0cvUUcyN3U0Z1dQUUxYUmUzOXdVRzFwMk52cGRTSzA4N05xaWdUT0ZPUUxFLmpwZw::/original_4k.jpg",
+    name: "Monster Hunt",
 },
 {
     id: "429",
     src: "https://bkrapi-win.v2.bookrclass.com/convertedBooks/ID429bc_20211210143829.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci85L2ovOWpqcFFGWmw0azBOU0FzVEFQVDI1Q0pqQ2VXOVhySGJzZUtBSGdEWHlOLmpwZw::/original_4k.jpg",
+    name: "Going to Boston",
 },
 {
     id: "2",
     src: "https://bkrapi-win.v2.bookrclass.com/convertedBooks/ID2_20211112100124.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci91LzcvdTdUNTVua0t6SnF1T1Q0dWNRQ0xUQVpyOWg2Qm5lQ0xLdVpBajJTTGxzLmpwZw::/original_4k.jpg",
+    name: "A Day At The Beach",
 },
 {
     id: "226",
     src: "https://bkrapi-win.v2.bookrclass.com/convertedBooks/ID226_20211117100709.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci9SL24vUm5vTGV1ZUNKQVMwMFNyaHRvb3FOU0oyU3NXNWZ4anFhTnZ2NTZqY2lHLmpwZw::/original_4k.jpg",
+    name: "The Magic Machine",
 },
 {
     id: "409",
     src: "https://bkrapi-win.v2.bookrclass.com/convertedBooks/ID409_20211116132454.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci9hLzUvYTVyZjVGd2ZGVVN2TWpKbXd4UWFMUFY5SDZXN000N0twSEFxNmZscmh1LmpwZw::/original_4k.jpg",
+    name: "Grumpy Pants",
 },
 {
     id: "406",
     src: "https://bookrlab.com/classapi/converter/convertedBook/converted/ID406_20211116131850.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci9rL0kva0k3dmRwUkNYZHJoZDZlanphTUplVlU2bmNoS29tbHMzbXZ6VHRUTktGLmpwZw::/original_4k.jpg",
+    name: "Thanksgiving",
 },
 
 // FIRST 8
@@ -44,41 +52,49 @@ var bookList = [{
     id: "18",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3L1IvdS9SdTBnc1hTMzdVbWhoQmZWa1puS0lZeENJQ3BBOHlHVXNCRFpPNlFwTmIubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci9WLzMvVjNlVFpreXpoNXFJMHowNFBrUEUxR2hkbDNBSXBhUXBGNk84dUdKWkZELmpwZw::/original_4k.jpg",
+    name: "Emma's Family",
 },
 {
     id: "84",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3L0MvYi9DYmwwaHFwWTczWE9pQVh4M1FsM01VSHRsczZKdzBEZVBrMnRGN2NMVW0ubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci95L1gveVhKNnNHN0NpdnFqUmx3aWZHNWdXTDFWR2FtNmxnZEJZUmU3WHpQNUFsLmpwZw::/original_4k.jpg",
+    name: "Getting Ready",
 },
 {
     id: "484",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3L3kvSi95SlQ4VU5OYXZJVlBIYjBxakkzNE01SXY0VGJmakMxbVBNOWpaZVA1ZXoubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci9JLzIvSTJRMFJIZnRmd280QnA2Y2VzS0xJRklEQWRHekRudTBYTUFLbENtQlVRLmpwZw::/original_4k.jpg",
+    name: "Theo's Mood",
 },
 {
     id: "15",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3LzQvSy80S1cxUHdhT0pRRkJXOHZnbkFXWm9UaG9CNklndnppeVVQV0tzMXRqeVAubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci9JLzYvSTZsNWNES0JuN2hoZWVBNmhSSVphUlQwZ1pzd3ZlY0hGSmtYU3hnSThDLmpwZw::/original_4k.jpg",
+    name: "At the Market",
 },
 {
     id: "371",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3L3IvZC9yZHZ1cTNuMGtKazJqVFFTWW4zM0MxOTZvZDlnRG1TNUZBUzJZaWE1VDkubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci9CL2gvQmhyYlJOb3F4ckM1ell2S3oxajY4TFZoZlQzUVdJMGd6M1NiZ1BXTGw0LmpwZw::/original_4k.jpg",
+    name: "Let's Ride Scooters!",
 },
 {
     id: "99",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3L3kvMC95MGd1djF0WklMTzdXMlNSTlhORnA1RTd4R0JuWmw4M0I0dHdmdUFCTG4ubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci8yLzYvMjZqTHF1czhEYTBFazZibmU3WDA3TVh0WmpTSFhxTGk3UDlLaTNyNXNOLmpwZw::/original_4k.jpg",
+    name: "Hamster Hunt",
 },
 {
     id: "413",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3L1UvTi9VTktyNXdqSFhwNFVEUU1lWmdkNHZUZktmbTZmS3FjSXJoUGtXNUZHTFMubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci9uL2EvbmE1V1NJZGdDWHJwQVNLZkY3d3VIRFZzUWdRRndiektkdXRXMGM0OVhxLmpwZw::/original_4k.jpg",
+    name: "When I Feel Good About Myself",
 },
 {
     id: "374",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3LzMvYy8zY2E0cVlWTGlyVGFYYktkSVhFaE4yd1d3S2JXZ1F2YXJKeVBCUFZYcTQubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci9qL08vak9PVW1EUW1UNzdXTkVNWExKY09hdHowNnU0QUk3UWIxaUx0cTVTNk1OLnBuZw::/original_4k.png",
+    name: "Traffic Jam",
 },
 
 // SECOND 8
@@ -86,41 +102,49 @@ var bookList = [{
     id: "135",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3L3ovWS96WXB5UGh4MEJrQ0F2SHJqVzV6dFEzOGczb1V2dkZNcGlnT0pibVZZcEsubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci9OLzkvTjlIMHk1SUFoTzZ4RTZ5UjJZa1plTmVPcEN0dGRqdHpHQjd0bXNicThsLmpwZw::/original_4k.jpg",
+    name: "Let's Go Outside!",
 },
 {
     id: "83",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3L2UvZS9lZUlxSHpvajBpSHhXQ1hCbkQ0djNJRmd0dEI1bTlGUnFwMlRubm53QTAubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci8zL3ovM3psTVRWSVJYUExTSjZUWlFVeldLSDlGcVl6dVpJc3BJWUlONDl2ODMyLmpwZw::/original_4k.jpg",
+    name: "Funny Fruits",
 },
 {
     id: "423",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3L0YvbC9GbDl5VmFoNUlZQ2tWSmlPS0pvelM5cHR0RkRteWt5MG83UlBuWU56VVYubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci9lL1YvZVZ2cHJQOWZRRkVRcUxCaHZWUUJaQVJkSDIzOGVFQ2ExMzcwVWd1UlcwLmpwZw::/original_4k.jpg",
+    name: "Belly Breathe",
 },
 {
     id: "361",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3L2MvZi9jZllFRm5OdXdqTWF0WXpPbVdrVldtR2xKdEx3YzdGNTNvdW41Y0FqdlQubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci83L1UvN1V1MHlpRjhISVJHTUc4cnNvWUZjTHd5S1JoTldzWFZWamdsOWlFMjFpLmpwZw::/original_4k.jpg",
+    name: "Are you ready for the lesson?",
 },
 {
     id: "125",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3L3MvRS9zRTlsTk90T1JlZlZaMjVOSkt1dEdqZU9qRVV0b2pJWjRyTGU4YUFHN1cubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci9wL3AvcHBoMGQ5bXdyY2V1dklQT3Y1dnhhWFVSeGZacFhMYmdrcGFOcVRZeTZILmpwZw::/original_4k.jpg",
+    name: "I want a tablet, please!",
 },
 {
     id: "229",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3L1IvaS9SaW5VRVF5dlc3eU02U3VCeVFxQ0hMYnRUQXNtQVE5UE55eWx3ZldhS2cubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci9OL1gvTlg4MnNkU0VrbDFsS2Y5VVpqcDBONkVKcFVsQ2xVWWNnOHpEaExjdXh6LmpwZw::/original_4k.jpg",
+    name: "The Lost Dog",
 },
 {
     id: "367",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3L3IvTC9yTGtGYUpLNUM1bk9mQ1pnYTVJUTdjT2F3Qms3emt5ejV2SlMwRGVNcGUubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci9tL0cvbUc4bzhZQ3ZyVE5Xa244VzFZVXBac0ZSbXhRR3ZrS3RicVBpZFNpY1ZVLmpwZw::/original_4k.jpg",
+    name: "Going to Washington",
 },
 {
     id: "23",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3L3ovNS96NTBTSXlid1lTWWxyUHVjOVNhNnlXeGNXS3FCak1XdWZwMElha3UzRlYubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci90L1MvdFNuVXBXNTN1c3JTdXg1TGpEU0N5Q2FCbVRCY2ZnaHMwZ2h3a3prb1FNLmpwZw::/original_4k.jpg",
+    name: "Guess Who!",
 },
 
 // THIRD 8
@@ -128,41 +152,49 @@ var bookList = [{
     id: "263",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3L3gvTi94TjE4ZXVzYU5Xc09LV0FRamtwQnd0UWNZamIzRkphMDkxaXJiblhFb0subXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci9iL2UvYmVWUEJDQmdvZE1obmFyZTRQckJkSEl6dEpMMTNDbjVZYnh4UjJlUzVqLmpwZw::/original_4k.jpg",
+    name: "Sam the Polar Bear",
 },
 {
     id: "102",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3L0wvby9Mb1dyUFZia29Zc3FQMnJjYzNzazQ3aFpVSHZRcWp0Y3Fya3R2eEZoRXEubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci90L0MvdENETWhzT1ZSeUxmVDloYUtZR3ZuUldpanl5UERPWUE3ZU5RbE82QVdGLmpwZw::/original_4k.jpg",
+    name: "Hello, Teddy!",
 },
 {
     id: "525",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3L2UvMy9lM1NoTWtVZHNqdnZ5Y213eUhPQ01QODk2ZlBLRGRKUWZldGFuUjFwRkEubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci83L0EvN0FBYjd5ODFRbllrQ3RESlZjNlRvUng2bFIxdUJmRDFYMkdyeUNBaGt3LmpwZw::/original_4k.jpg",
+    name: "Girls Can Do Anything",
 },
 {
     id: "127",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3L24vMS9uMXNQT09DcGZlWTFQTHQ1dkZNdGVnUHFZZm85REZOY2llbjRoSFBiOWIubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci9lL2QvZWRDY3d5ZHJFTFNOYmRSSHJEUFVJZjQydDQwQVJFTjl3RDJzdmRnNFZ4LmpwZw::/original_4k.jpg",
+    name: "In the Garden",
 },
 {
     id: "385",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3L08vTi9PTmFPTkRjYmg2S1lheko0eTVRWFNzQXNEOWRockVKQmF5VFZVenppWlAubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci91L0IvdUJueFdHSTJsM1haaVZzcnkzSUNRN2VIUmRBRzk0Ull5Zm5VRHFlSFBKLmpwZw::/original_4k.jpg",
+    name: "The Prince's Tooth Is Loose",
 },
 {
     id: "366",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3L1AvRi9QRk1yN0tIWGFiVmhQSGNhZTZRVm5aTE9KdTJnQ1JTRG13a1dNN2RjRHYubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci9PL0MvT0NKRmVLRk9aa0JMSDJUQmZsNWhKV1BucG9mTVF1RWhpMkMzd2RJbkhNLmpwZw::/original_4k.jpg",
+    name: "Father's Day",
 },
 {
     id: "73",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3L1MvWC9TWE9Sa1o0eDQyTDNrVE9KUjdBcXg4NzFIelZCWGJJNUtRNnNHbGRhVXYubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci9yL2cvcmdEM2ZFcUp1Y2lTektpRnVjaElLTG5zSFJLTDM1eXRFSnFZQThzMUt4LmpwZw::/original_4k.jpg",
+    name: "Bring Your Pet to School",
 },
 {
     id: "213",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3L2YvMy9mMzQ5bUczUTd3WHZyRTNhRlRSdU9EbFQ3bUZHNWVHaWtUSVBsUXpHc0YubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci9FL2QvRWQzMEY5Z2QwS2FnSE9wRDBkbDNJa01rUHcyUkpJejNnbUxMQ05GM2R4LmpwZw::/original_4k.jpg",
+    name: "The Tortoise and the Hare",
 },
 
 // LAST 8
@@ -170,40 +202,48 @@ var bookList = [{
     id: "91",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3L0YvYi9GYkdrUzRNeVVHUXdWRklOUVFuamZtVTJpRmpTc3RrT1ZwR2xla09UOG0ubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci85L1cvOVdQVWE2N3VTSzM0b2NIdFFMNlQyWG5CTlVWOUl3elI4S1g0Mk1CYjRLLmpwZw::/original_4k.jpg",
+    name: "Wiggling Veggies",
 },
 {
     id: "180",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3L0ovWi9KWko0Sm95aGVPaDRzSFA2VU9hcjZQQzdwU1ByamtreThOdjhnZ3B5Nm4ubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci9oL3UvaHVUNkZ6Nm5GNFlGNzBoQm1oZllCZ1UxSlZWNWdLTzd3a05tYm5KSTdLLmpwZw::/original_4k.jpg",
+    name: "What's This?",
 },
 {
     id: "13",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3L3YvYi92YkxEWTNnNzlwekRrRERWRUlVeHJwRkN0Y2N5N2ZLZEtlTTlaWEpaWUEubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci9ZL20vWW1nUDMxYW92WWFOTzF6YUtycGJNdmJ6S0ZWd1RpMVhWaHl4ODE5UFhPLmpwZw::/original_4k.jpg",
+    name: "Amy and Tom",
 },
 {
     id: "375",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3L3MvOC9zOG5sd29FMFFlUzM2ekVWblptdnRwbGdYNkpUYkZvdm5ubklKRXkyRjQubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci85L1gvOVh2STljZ1JhbUxQSElYTEM1UW4zS2Z2T1dJaFJHWHZFYlByN0lyNnpzLmpwZw::/original_4k.jpg",
+    name: "Who Are They?",
 },
 {
     id: "381",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3L2kvbS9pbXYwSzIxUkR4VUdVcjJ0UnJkUnMyTUZrM0JFaW9TZmxkcTlKYlB3dTgubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci90LzQvdDRFaGVMUVB1ODVXQWZKa3NPOXhvMjhJMjR6Z1p0ZHVkZ2NOblpQVU5SLmpwZw::/original_4k.jpg",
+    name: "Say Cheese!",
 },
 {
     id: "373",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3LzAvQy8wQ1EzcXBHVWNlQkg5c01oaTdGRDhzamFYM3FGYTlvZ21DVlZiNWhmRzIubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci94L0sveEtQeUhFekxVWFN4RnRGdjY1bk51aEp2UVV2U2Jld2pwbVBkcmswUkttLmpwZw::/original_4k.jpg",
+    name: "Saving the Unicorn",
 },
 {
     id: "363",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3LzQvNC80NFJSMTdUOUNiOW5SMllheUJJbFdUU3NQd1hsV3hrMGV0Q2Fyc1E3Rm4ubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci9vL0Uvb0VJbktiUmtDakxRZjlCTVUyS1A2czdYb2M3VUFQUFFHWW5jd0JVSHBhLmpwZw::/original_4k.jpg",
+    name: "Class Play",
 },
 {
     id: "365",
     src: "https://api.v2.bookrclass.com/api/media/Ym9vay1wcmV2aWV3L3AvOS9wOUxGelJTbHlOYk9zOGNhblNmdzZDbXNWYlNnOTh5RUY2RnFWQkRKcWgubXA0/original.mp4",
     posterImg: "https://api.v2.bookrclass.com/api/media/Ym9vay1jb3Zlci9YLzkvWDlBVTdDdlZCcUhRcGVCWjNWYnRjQ0tMYVdrQjhxcmZ2bDlZQmtmOFVSLmpwZw::/original_4k.jpg",
+    name: "Don't Let Auntie Mabel Bless the Table",
 }
 ]
